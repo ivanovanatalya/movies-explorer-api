80697d681bd6c3640e7e730dc9e64bcfe98544ee
Natasha Ivanova <terehinann@gmail.com>
2023-02-28 18:01:22 +0300

    feat: change server

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,13 @@
 // controllers/users.js
 // это файл контроллеров
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken'); // импортируем модуль jsonwebtoken
 const { default: mongoose } = require('mongoose');
 const User = require('../models/users');
 const {
   GeneralError,
   NotFoundError,
+  DataConflictError,
 } = require('../middlewares/errors');
 const { SECRET_KEY_DEV } = require('../constants');
 
@@ -19,6 +21,35 @@ const getCurrentUser = (req, res, next) => {
       return res.send(user);
     })
     .catch(next);
+};
+
+const createUser = (req, res, next) => {
+  const {
+    name,
+    email,
+    password: pass,
+  } = req.body;
+
+  bcrypt.hash(pass, 10)
+    .then((hashPass) => User.create({
+      name,
+      email,
+      password: hashPass,
+    })
+      .then((user) => {
+        const { _doc: { password, ...userRes } } = user;
+        res.send(userRes);
+      })
+      .catch((err) => {
+        if (err instanceof mongoose.Error.ValidationError) {
+          return next(new GeneralError('Переданы некорректные данные при создании пользователя'));
+        }
+
+        if (err.code === 11000) {
+          return next(new DataConflictError('e-mail already exists'));
+        }
+        return next(err);
+      }));
 };
 
 const updateUser = (req, res, next) => {
@@ -59,6 +90,7 @@ const login = (req, res, next) => {
 
 module.exports = {
   getCurrentUser,
+  createUser,
   updateUser,
   login,
 };
